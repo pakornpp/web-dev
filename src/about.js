b2b442b0b47617b6bdc9513b8b44f0d820e5f7f0
Pakorn Poksawat <pakorn.poksawat@gmail.com>
2026-05-23 21:53:41 +0700

    feat: lots of changes

--- src/about.js
+++ src/about.js
@@ -2,10 +2,12 @@ import "./styles.css";
 import { initLanguage, setLanguage } from "./i18n.js";
 import { initNav } from "./nav.js";
 import { initFooter } from "./footer.js";
+import { initCtaBanner } from "./cta-banner.js";
 
 // Expose setLanguage globally for lang-switcher button onclick handlers
 window.setLanguage = setLanguage;
 
 initNav("index.html");
 initFooter("index.html");
+initCtaBanner();
 initLanguage();
